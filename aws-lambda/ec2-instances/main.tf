0806a058ecbb2929abf4e52ed2f1f62e0856d19d
alcarlucci <alcarlucci02@gmail.com>
2022-05-30 09:38:22 -0300

    meu commit

--- aws-lambda/ec2-instances/main.tf
+++ aws-lambda/ec2-instances/main.tf
@@ -13,7 +13,11 @@ provider "aws" {
 }
 
 resource "aws_security_group" "teste_lambda_instances" {
-  name    = "teste_lambda_instances"
+  name    = "nsg-teste_lambda_instances"
+  tags = {
+    Name = "nsg-teste_lambda_instances",
+    Projeto = "mod-03"
+  }
 }
 
 ############ Inbound Rules ############
@@ -38,38 +42,42 @@ resource "aws_security_group_rule" "app_server_sg_outbound" {
 
 resource "aws_instance" "teste_lambda_01" {
   ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t3a.small"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [ "${aws_security_group.teste_lambda_instances.id}" ]
   tags = {
-    Name = "Teste Lambda 01",
+    Name = "vm-Lambda-01",
+    Projeto = "mod-03",
     Desliga = "True"
   }
 }
 
 resource "aws_instance" "teste_lambda_02" {
   ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t3a.small"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [ "${aws_security_group.teste_lambda_instances.id}" ]
   tags = {
-    Name = "Teste Lambda 02",
+    Name = "vm-Lambda-02",
+    Projeto = "mod-03",
     Desliga = "True"
   }
 }
 
 resource "aws_instance" "teste_lambda_03" {
   ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t3a.small"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [ "${aws_security_group.teste_lambda_instances.id}" ]
   tags = {
-    Name = "Teste Lambda 03",
+    Name = "vm-Lambda-03",
+    Projeto = "mod-03"
   }
 }
 
 resource "aws_instance" "teste_lambda_04" {
   ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t3a.small"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [ "${aws_security_group.teste_lambda_instances.id}" ]
   tags = {
-    Name = "Teste Lambda 04",
+    Name = "vm-Lambda-04",
+    Projeto = "mod-03"
   }
 }
